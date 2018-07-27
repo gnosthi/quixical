b2c04a14680542c142197f6c95756e2555dc41b6
Kenneth Jordan <kenneth.jordan@pepsico.com>
2018-07-27 11:03:01 -0500

    added benchmarks to main_test.go

--- main_test.go
+++ main_test.go
@@ -3,18 +3,32 @@ package main
 import (
 	"flag"
 	"testing"
+    "os"
+    "os/exec"
 )
 
+var csvFile = flag.String("csv", "test_problems.csv", "csv test")
+
 func TestReadFile(t *testing.T) {
-	csvFile := flag.String("csv", "test_problems.csv", "csv test")
-	lines, err := readFile(csvFile)
+    doRandom := false
+	lines, err := readFile(csvFile, doRandom)
 	if err != nil {
 		t.Error(err)
 	}
 	switchQAndA(t, lines)
 }
 
+func BenchmarkReadFile(t *testing.B) {
+    doRandom := false
+    lines, err := readFile(csvFile, doRandom)
+    if err != nil {
+        t.Error(err)
+    }
+    benchSwitchQAndA(t, lines)
+}
+
 func TestParseLines(t *testing.T) {
+    doRandom := false
 	problems := [][]string{
 		{"Question?", "Answer"},
 		{"1+2", "3"},
@@ -22,8 +36,21 @@ func TestParseLines(t *testing.T) {
 		{"Alice", "Bob"},
 		{"Mock", "Spock"},
 	}
-	lines := parseLines(problems)
+	lines := parseLines(problems, doRandom)
 	switchQAndA(t, lines)
+}
+
+func BenchmarkParseLines(t *testing.B) {
+    doRandom := false
+    problems := [][]string{
+        {"Question?", "Answer"},
+        {"1+2", "3"},
+        {"Foo", "Bar"},
+        {"Alice", "Bob"},
+        {"Mock", "Spock"},
+    }
+    lines := parseLines(problems, doRandom)
+    benchSwitchQAndA(t, lines)
 }
 
 func switchQAndA(t *testing.T, lines []problemSet) {
@@ -53,8 +80,39 @@ func switchQAndA(t *testing.T, lines []problemSet) {
 	}
 }
 
+func benchSwitchQAndA(t *testing.B, lines []problemSet) {
+    for i, p := range lines {
+        if len(lines) <= 1{
+            t.Errorf("Total number of questions is %d. Expected %d", i, 5)
+        }
+        switch i {
+        case 0:
+            benchQAParseError(t, "Question?", p.question)
+            benchQAParseError(t, "Answer", p.answer)
+        case 1:
+            benchQAParseError(t, "1+2", p.question)
+            benchQAParseError(t, "3", p.answer)
+        case 2:
+            benchQAParseError(t, "Foo", p.question)
+            benchQAParseError(t, "Bar", p.answer)
+        case 3:
+            benchQAParseError(t, "Alice", p.question)
+            benchQAParseError(t, "Bob", p.answer)
+        case 4:
+            benchQAParseError(t, "Mock", p.question)
+            benchQAParseError(t, "Spock", p.answer)
+        }
+    }
+}
+
 func qaParseError(t *testing.T, expected, recieved string) {
 	if expected != recieved {
 		t.Errorf("Expected %s, got %s", expected, recieved)
 	}
+}
+
+func benchQAParseError(t *testing.B, expected, recieved string) {
+    if expected != recieved {
+        t.Errorf("Expected %s, got %s", expected, recieved)
+    }
 }
